8e6679bcac674cf9d4361737805667632b5c7ded
João Paulo Campos <62832658+joaopaulocampospereira@users.noreply.github.com>
2023-11-23 17:07:29 -0300

    add-firebase-configs

---
+++ lib/controller/task_controller.dart
@@ -0,0 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import '../model/tarefa.dart';
+import '../view/util.dart';
+import 'login_controller.dart';
+
+class TarefaController {
+  //
+  // ADICIONAR uma nova Tarefa
+  //
+  void add(context, Contact c) {
+    FirebaseFirestore.instance
+        .collection('contact')
+        .add(t.toJson())
+        .then((value) => sucesso(context, 'Contato adicionado com sucesso'))
+        .catchError(
+            (e) => erro(context, 'Não foi possível adicionar o contato.'))
+        .whenComplete(() => Navigator.pop(context));
+  }
+
+  //
+  // ATUALIZAR
+  //
+  void update(context, id, Contact c) {
+    FirebaseFirestore.instance
+        .collection('contact')
+        .doc(id)
+        .update(t.toJson())
+        .then((value) => sucesso(context, 'Contato atualizado com sucesso'))
+        .catchError(
+            (e) => erro(context, 'Não foi possível atualizar o contato.'))
+        .whenComplete(() => Navigator.pop(context));
+  }
+
+  //
+  // EXCLUIR
+  //
+  void exclude(context, id) {
+    FirebaseFirestore.instance
+        .collection('contact')
+        .doc(id)
+        .delete()
+        .then((value) => sucesso(context, 'Contato excluído com sucesso'))
+        .catchError((e) => erro(context, 'Não foi possível excluir o contato.'));
+  }
+
+  //
+  // LISTAR todas as Tarefas da coleção
+  //
+  list() {
+    return FirebaseFirestore.instance
+        .collection('contact')
+        .where('uid', isEqualTo: LoginController().idUsuario());
+  }
+}
